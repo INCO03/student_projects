55725847f81e703af7306d57d7c1ce7a8e0e49ef
INCO03 <irekzborozlagowski@gmail.com>
2024-02-26 09:52:34 +0000

    studentmachine send

--- cpp/liczbyPierwsze/main.cpp
+++ cpp/liczbyPierwsze/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 
 using namespace std;
-bool czy_pierwsza(int n)
-{
-if(n<2)
+bool czy_pierwsza(int liczba){
+if (liczba<=1){
 return false;
-for(int i=2;i*i;i++)
-if(n%i==0)
+}
+for(int i=2;i*i<=liczba;++i){
+if(liczba %i==0){
 return false;
+}
+}
 return true;
 }
-int main()
-{
-int n;
-cout<<"podaj liczbe:";
-cin>>n;
-if(czy_pierwsza(n))
-cout<<"liczba"<<n<<"jest pierwsza"<<endl;
-else
-cout<<"liczba"<<n<<"nie jest pierwsza"<<endl;
-
+int main(){
+cout<<" liczby pierwsze:"<<endl;
+int liczba=2;
+int licznik=0;
+while(licznik<200){
+if(czy_pierwsza(liczba)){
+cout<<liczba<<"";
+++licznik;
+}
+++liczba;
+}cout <<endl;
 return 0;
 }
