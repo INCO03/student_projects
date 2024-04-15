cc98487a82c15127c172b08e4b6a039455231f2e
INCO03 <irekzborozlagowski@gmail.com>
2024-04-15 09:19:54 +0000

    studentmachine send

---
+++ python/cw_test/main.py
@@ -0,0 +1,12 @@
+print ("witaj swiecie")
+x=5
+y=6
+print(y)
+if x<y:
+    print("x jest mniejsze od y")
+elif y<x:
+    print("x jest wiekszy od y")
+
+a=int(input("podaj a"))
+b=int(input("podaj b"))
+print(a+b)
